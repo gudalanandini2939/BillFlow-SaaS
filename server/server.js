a22a8af004a397615ca6529e048e8b9c34d4ee25
Gudala Nandini <gudalanandini2939@gmail.com>
2026-06-11 01:12:36 +0530

    Fix CORS preflight

--- server/server.js
+++ server/server.js
@@ -7,32 +7,38 @@ const authRoutes = require("./routes/authRoutes");
 const invoiceRoutes = require("./routes/invoiceRoutes");
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
 
-// CORS
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://bill-flow-saa-s.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-// Middleware
+app.options("*", cors());
+
 app.use(express.json());
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("BillFlow API Running...");
 });
 
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/invoices", invoiceRoutes);
-
-// Start Server
-const PORT = process.env.PORT || 5000;
-
 app.get("/api/auth-test", (req, res) => {
   res.json({ message: "Auth routes loaded correctly" });
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/invoices", invoiceRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
