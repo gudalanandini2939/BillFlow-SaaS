35ced21cebcac0cd08818297bbcf87b4b99cce57
Gudala Nandini <gudalanandini2939@gmail.com>
2026-06-11 00:25:48 +0530

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -1,34 +1,33 @@
-const invoiceRoutes = require("./routes/invoiceRoutes");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const invoiceRoutes = require("./routes/invoiceRoutes");
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://bill-flow-saa-s.vercel.app",
-    ],
-    credentials: true,
-  })
-);
+// CORS
+app.use(cors());
 
+// Middleware
 app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("BillFlow API Running...");
 });
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/invoices", invoiceRoutes);
 
+// Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
