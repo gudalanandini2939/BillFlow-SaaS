10fc31bcb85d2812ba55fbf6b5b9eec52995d77c
Gudala Nandini <gudalanandini2939@gmail.com>
2026-06-11 00:13:23 +0530

    Fix backend route order and CORS

--- server/server.js
+++ server/server.js
@@ -6,24 +6,31 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://bill-flow-saa-s.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("BillFlow API Running...");
 });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/invoices", invoiceRoutes);
+
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/invoices", invoiceRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-app.use("/api/auth", authRoutes);
