18d82039a3935cb5282a8396f0a66981a7a92fad
Gudala Nandini <gudalanandini2939@gmail.com>
2026-06-10 23:51:59 +0530

    Connect frontend to Render backend

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL ||"https://billflow-saas-oxxp.onrender.com";
 
 function UserDashboard() {
   const [invoices, setInvoices] = useState([]);
